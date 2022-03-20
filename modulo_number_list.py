4eca26a6f3f34cf7e4680e048d0ae5bbcc8dfcb5
Awsamo0190 <99389724+Awsamo0190@users.noreply.github.com>
2022-03-20 14:33:06 -0400

    Create modulo_number_list.py

---
+++ modulo_number_list.py
@@ -0,0 +1,5 @@
+numbers = [145, 126, 356, 272, 98, 378, 395, 352, 392, 215, 446, 168, 180, 359, 51, 190, 404, 209, 185, 115, 363, 431, 103]
+#The list of numbers to get the remainder of, this can can be substituted for any list of numbers           
+for number in numbers:
+  print(int(number) % 41) 
+  # Gives the remainder of each number after being dived by 41 you can substitute that for any number
