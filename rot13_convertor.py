bf98645a24673decbc3d8e0b1a758dcbe37be047
Awsamo0190 <99389724+Awsamo0190@users.noreply.github.com>
2022-03-25 03:29:13 -0400

    Create rot13_convertor.py

---
+++ rot13_convertor.py
@@ -0,0 +1,4 @@
+import codecs
+from codecs import encode
+what_2_conv = input("What are we decoding?") #Variable that will ask for rot13 cipher to decode this will also encode to rot13
+conv_txt = print(encode(what_2_conv, 'rot_13')) #Variable to print the converted text
