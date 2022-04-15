da9ba5438ee3e9b184798b6c373b0fc1b348219f
Awsamo0190 <99389724+Awsamo0190@users.noreply.github.com>
2022-04-15 06:13:15 -0400

    Update modulo_number_list.py

--- modulo_number_list.py
+++ modulo_number_list.py
@@ -1,3 +1,5 @@
+#!/bin/python
+
 numbers = [145, 126, 356, 272, 98, 378, 395, 352, 392, 215, 446, 168, 180, 359, 51, 190, 404, 209, 185, 115, 363, 431, 103]
 #The list of numbers to get the remainder of, this can can be substituted for any list of numbers           
 for number in numbers:
